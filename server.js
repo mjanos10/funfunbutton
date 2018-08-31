f1890e923f807f976378e88168f937ef16f2a590
janos.mayer <hq@mywebstuff.org>
2018-08-31 18:46:36 +0200

    Remove arg var usage and Array.from

--- server.js
+++ server.js
@@ -3,12 +3,12 @@
   this.clicks = 0
   this.app = require('express')()
   this.app.get('/', function() {
-    Array.from(arguments)[1].sendFile(__dirname + '/index.html')
+    arguments[1].sendFile(__dirname + '/index.html')
   })
 
   function clickHandler() {
-    Array.from(arguments)[0].query.clicked && this.clicks++
-    return Array.from(arguments)[1].json({ clicks: this.clicks })
+    arguments[0].query.clicked && this.clicks++
+    return arguments[1].json({ clicks: this.clicks })
   }
 
   this.app.get('/clicked', clickHandler.bind(this))
